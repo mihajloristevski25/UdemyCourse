81bfa171821482f8a9d8b7edfefc5abb7d72d663
Mihajlo Ristevski <80358616+mihajloristevski25@users.noreply.github.com>
2021-12-14 03:38:14 +0100

    Add files via upload

---
+++ Collections/src/com/mihajlo/Main.java
@@ -0,0 +1,68 @@
+package com.mihajlo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.StreamHandler;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian", 8 ,12);
+
+
+        if(theatre.reserveSeat("A02")){
+            System.out.println("Please pay for A02");
+        }else {
+            System.out.println("seat is already reserved");
+        }
+
+        if(theatre.reserveSeat("C12")){
+            System.out.println("Please pay for A02");
+        }else {
+            System.out.println("seat is already reserved");
+        }
+
+        if(theatre.reserveSeat("B02")){
+            System.out.println("Please pay for A02");
+        }else {
+            System.out.println("seat is already reserved");
+        }
+        if(theatre.reserveSeat("D04")){
+            System.out.println("Please pay for A02");
+        }else {
+            System.out.println("seat is already reserved");
+        }
+        if(theatre.reserveSeat("A02")){
+            System.out.println("Please pay for A02");
+        }else {
+            System.out.println("seat is already reserved");
+        }
+
+
+        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+        printList(reverseSeats);
+
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00" , 13.00));
+        priceSeats.add(theatre.new Seat("A00" , 13.00));
+        Collections.sort(priceSeats,Theatre.PRICE_ORDER);
+        printList(priceSeats);
+    }
+
+    public static void printList(List<Theatre.Seat> list){
+        for(Theatre.Seat seat : list){
+            System.out.print(" " +seat.getSeatNumber() + " $" + seat.getPrice());
+        }
+        System.out.println();
+        System.out.println("==========================================================");
+    }
+
+
+
+
+
+
+}
